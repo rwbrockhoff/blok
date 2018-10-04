62600706f0cf63277c940d84bfd054738c20bf27
Ryan Brockhoff <brockhoffrw@gmail.com>
2018-10-04 02:29:05 -0600

    RouterButton installed. Fonts installed for later use w/link.

--- components/Home/Home.android.js
+++ components/Home/Home.android.js
@@ -1,12 +1,15 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react'
+import { Text, View } from 'react-native'
+import RouterButton from 'react-router-native-button'
+
 import styles from './Home.styles'
 
 export default class Home extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Home</Text>
+        <Text style={styles.font}>blok</Text>
+        <RouterButton to="/" title="start blok" color="#841584"/>
       </View>
     );
   }
--- components/Home/Home.styles.js
+++ components/Home/Home.styles.js
@@ -2,9 +2,18 @@ import {StyleSheet} from 'react-native'
 
 export default styles = StyleSheet.create({
     container: {
-      backgroundColor: 'beige',
+      backgroundColor: 'black',
       flex: 1,
       alignItems: 'center',
       justifyContent: 'center',
     },
+    font: {
+      fontFamily: 'Roboto',
+      fontWeight: 'bold',
+      color: 'white',
+      fontSize: 60
+    },
+    customButton: {
+      width: 150
+    }
   });
